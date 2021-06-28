7f7f752d073a34dc2e2e8a83f67736e0dc17656e
= <nice.mutnykh@mail.ru>
2021-06-28 21:59:14 +0300

    sort posts

--- js/details.js
+++ js/details.js
@@ -5,10 +5,12 @@ const render = async () => {
   const res = await fetch('http://localhost:3000/posts/' + id) // fetch single post
   const post = await res.json();
 
+  let date = new Date(post.date).toDateString()
   const template = `
   <article>
       <div>
         <h2>${post.title}</h2>
+        <p class="date"><small>${date}</small></p>
         <div class="content">${post.content}</div>
       </div>
     </article>
--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 let container = document.querySelector('.articles')
 
 const render = async () => {
-  let url = 'http://localhost:3000/posts'; //endpoint
+
+  // fistly, sort by date value
+  // then, sort by desc parametr 
+  let url = 'http://localhost:3000/posts?_sort=date&_order=desc'; //endpoint
 
   const res = await fetch(url);
   const posts = await res.json(); // store all data inside posts
   //console.log(posts)
-  
+
   let template = ''
   posts.forEach(post => {
+    let date = new Date(post.date).toDateString()
     template += `
     <article>
       <div>
         <h2>${post.title}</h2>
+        <p class="date"><small>${date}</small></p>
         <div class="content">${post.content.slice(0, 200)}</div>
         <button onclick="window.location.href = '/details.html?id=${post.id}'">read more...</button>
       </div>
