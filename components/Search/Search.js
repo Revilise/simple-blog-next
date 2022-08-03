1ed11787d786d9fb9dd808d6ea920b8e92ef2db1
Revilise <nice.mutnykh@mail.ru>
2022-08-03 16:14:15 +0300

    search layout

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,7 +1,13 @@
+import classes from './Search.module.scss'
+import Icons from "../SvgIcons/SvgIcons";
+
 export default function Search() {
     return (
-        <div>
-            <input type={"text"} />
+        <div className={classes.container}>
+            <input placeholder={"search everything"} type={"text"} className={classes.input} />
+            <button className={classes.button}>
+                <Icons.Search />
+            </button>
         </div>
     )
 }
