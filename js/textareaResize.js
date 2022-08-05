a018fe46a589208af961978b145863fc17d2577d
Revilise <nice.mutnykh@mail.ru>
2022-08-05 15:59:21 +0300

    Update textareaResize.js

--- js/textareaResize.js
+++ js/textareaResize.js
@@ -1,9 +1,6 @@
 let textarea = document.querySelector('textarea')
 
 textarea.addEventListener('input', event => {
-  console.log(event.target.style.height)
   event.target.style.height = "auto"
-  console.log(event.target.style.height)
   event.target.style.height = event.target.scrollHeight + 16 + "px"
-  console.log(event.target.style.height)
 })
