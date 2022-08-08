fe7c4599bf15e38029dd5cc8207bb7b3c2c7d4a3
Revilise <nice.mutnykh@mail.ru>
2022-08-08 14:48:19 +0300

    tr

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,24 @@ import Button from "../components/Button/Button";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Main from "../components/Main/Main";
 import Search from "../components/Search/Search";
+import {useEffect, useState} from "react";
 
-export async function getStaticProps({params}) {
-    const posts = await postsController.getAll()
-    return {
-        props: {
-            posts
-        }
-    }
-}
+// export async function getStaticProps({params}) {
+//     const posts = await postsController.getAll()
+//     return {
+//         props: {
+//             posts
+//         }
+//     }
+// }
 
-export default function Home({posts}) {
+export default function Home() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        postsController.getAll().then(res => setPosts(res));
+    },[])
+
     return (
         <Layout title={"home"}>
             <Header search>
