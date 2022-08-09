e0461585e9be212db067c12f5a7bc673e82e3b32
Revilise <nice.mutnykh@mail.ru>
2022-08-09 15:18:33 +0300

    fix btns props

--- components/Link/Link.js
+++ components/Link/Link.js
@@ -5,25 +5,22 @@ function LinkHOC (props) {
     const router = useRouter();
     const Component = props.component;
 
-    function redirect (e) {
+    function onClick(e) {
         e.preventDefault();
         router.push(props.href);
     }
 
-    return <Component {...props} redirect={redirect} />
+    return <Component {...props} onClick={onClick} />
 }
 
 const StringLink = (props) => {
     const data = {...props};
-    data.onClick = props.redirect;
-
-    delete data.redirect;
     delete data.component;
 
     return <a href={"#"} {...data} />
 }
 
-const ButtonLink = (props) => <Button {...props} onClick={props.redirect} />
+const ButtonLink = (props) => <Button {...props} />
 
 ////// export
 const Link = (props) => <LinkHOC {...props} component={StringLink} />;
