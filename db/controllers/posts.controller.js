6ed0022bb917edc021dec8a0445d6345b784f8d8
Revilise <nice.mutnykh@mail.ru>
2022-08-19 17:20:20 +0300

    controller refactoring

--- db/controllers/posts.controller.js
+++ db/controllers/posts.controller.js
@@ -6,22 +6,31 @@ class PostsController {
     dbInstance = null;
     constructor() {
        this.dbInstance = collection(database, 'posts');
+
+       this.processData = (doc) => {
+            const data = doc.data();
+            const date = new Date(data.date);
+            const content = JSON.parse(data.content);
+            const id = doc.id;
+
+            return {...data, date, content, id};
+        }
     }
+
     getOne = (url) => {
         const q = query(this.dbInstance, where("url", "==", url))
         return getDocs(q)
             .then(res => {
                     const doc = res.docs[0];
                     if (doc) {
-                        const data = doc.data();
-                        const date = new Date(data.date);
-                        const content = JSON.parse(data.content);
-                        return {...data, date, content, id: doc.id};
+                        return this.processData(doc);
                     }
-                    return {empty: true}
+
+                    return null;
                 }
             )
     }
+
     getByString = (str, lastSnapshot, setLastSnapshot) => {
         return this.getAll({lastSnapshot, size: 100, setLastSnapshot})
             ?.then(res => res.filter(data => {
@@ -31,6 +40,7 @@ class PostsController {
                 })
             )
     }
+
     getAll({lastSnapshot, size, setLastSnapshot}) {
         const _limit = size ?? 5;
         if (lastSnapshot) {
@@ -41,15 +51,15 @@ class PostsController {
                 if (setLastSnapshot) setLastSnapshot(res.docs[res.docs.length - 1]);
 
                 return res.docs.map((item) => {
-                    const data = item.data();
-                    const contentObj = JSON.parse(data.content)
-                    const content = contentObj.blocks[0].text.substring(0, 255) + "..."
-                    return {...data, content}
+                    const data = this.processData(item);
+                    const content = data.content.blocks[0].text.substring(0, 255) + "...";
+                    return {...data, content }
                 });
             })
         }
         return null;
     }
+
     post = ({title, content}) => {
         const date = Number(new Date());
 
@@ -59,6 +69,7 @@ class PostsController {
         const url = `${translateRuEn(title)}_${date}`
         return addDoc(this.dbInstance, {title, content: JSON.stringify(content), date, url})
     }
+
     deleteById = async (id) => {
         const ref = doc(database, "posts", id)
         return deleteDoc(ref)
