429d9a8ba22db3f0febb2d9485a1133a6c417256
Revilise <nice.mutnykh@mail.ru>
2022-08-24 22:46:26 +0300

    memo editor

--- components/Editor/Editor.js
+++ components/Editor/Editor.js
@@ -1,3 +1,4 @@
+import React from "react";
 import createImagePlugin from '@draft-js-plugins/image';
 import Editor from '@draft-js-plugins/editor';
 import {useCallback} from "react";
@@ -5,22 +6,24 @@ import {useCallback} from "react";
 const imagePlugin = createImagePlugin();
 const plugins = [imagePlugin]
 
-export default function(props) {
-    const styleMap = {
-        'STRIKETHROUGH': {
-            textDecoration: 'line-through',
-        },
-    };
+export default React.memo(function(props) {
+        const styleMap = {
+            'STRIKETHROUGH': {
+                textDecoration: 'line-through',
+            },
+        };
 
-    // todo: different blocks styling
-    const blockStyleFn = useCallback(() => 'block', [])
+        // todo: different blocks styling
+        const blockStyleFn = useCallback(() => 'block', [])
 
-    return (
-        <Editor
-            {...props}
-            plugins={plugins}
-            blockStyleFn={blockStyleFn}
-            customStyleMap={styleMap}
-        />
-    )
-}
+        return (
+            <Editor
+                {...props}
+                onChange={props.onChange ?? function() {}}
+                plugins={plugins}
+                blockStyleFn={blockStyleFn}
+                customStyleMap={styleMap}
+            />
+        )
+    }
+)
